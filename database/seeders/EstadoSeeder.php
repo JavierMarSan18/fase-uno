681f411aafeccb55ae29650474e50c8c89beca8d
Javier Marroquín <Javier Marroqun>
2024-06-02 01:24:24 -0600

    Se creo seeder para estados

---
+++ database/seeders/EstadoSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Estado;
+
+class EstadoSeeder extends Seeder
+{
+    public function run()
+    {
+        $estados = [
+            ['nombre' => 'Recibido', 'orden' => 1],
+            ['nombre' => 'Reparado', 'orden' => 2],
+            ['nombre' => 'Finalizado', 'orden' => 3],
+            ['nombre' => 'Entregado', 'orden' => 4],
+        ];
+
+        foreach ($estados as $estado) {
+            Estado::create($estado);
+        }
+    }
+}
